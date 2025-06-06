fb5b4890d5501f219769efe0fd0046b24a3087a6
Kanny21 <christinaphessane@gmail.com>
2025-06-06 17:44:47 +0200

    Criacao da classe Especialidade

---
+++ src/test/java/com/agendamento/model/Especialidade.java
@@ -0,0 +1,17 @@
+package com.agendamento.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Especialidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(nullable = false, unique = true)
+    private String nome;
+    
+    private String descricao;
+}
