3e3753a7022a8139b02e1075c581b4484b586e83
Kanny21 <christinaphessane@gmail.com>
2025-06-06 20:21:12 +0200

    Criacao do repositório para a classe Consulta

---
+++ src/test/java/com/agendamento/repository/ConsultaRepository.java
@@ -0,0 +1,17 @@
+package com.agendamento.repository;
+
+import com.agendamento.model.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface ConsultaRepository extends JpaRepository<Consulta, Integer> {
+    List<Consulta> findByPacienteId(Integer pacienteId);
+    List<Consulta> findByMedicoId(Integer medicoId);
+    List<Consulta> findByStatus(Consulta.StatusConsulta status);
+    boolean existsByMedicoIdAndDataHoraInicioBetween(
+        Integer medicoId, LocalDateTime inicio, LocalDateTime fim);
+}
