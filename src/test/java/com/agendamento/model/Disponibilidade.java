7efb0c9b84717b84fe0a29b6cd0587598b85c973
Kanny21 <christinaphessane@gmail.com>
2025-06-06 18:48:47 +0200

    Criacao da classe Disponibilidade

---
+++ src/test/java/com/agendamento/model/Disponibilidade.java
@@ -0,0 +1,33 @@
+package com.agendamento.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalTime;
+
+@Data
+@Entity
+public class Disponibilidade {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @ManyToOne
+    @JoinColumn(name = "medico_id", nullable = false)
+    private Utilizador medico;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DiaSemana dia_semana;
+    
+    @Column(nullable = false)
+    private LocalTime horario_inicio;
+    
+    @Column(nullable = false)
+    private LocalTime horario_fim;
+    
+    public enum DiaSemana {
+        SEG, TER, QUA, QUI, SEX, SAB, DOM
+    }
+}
