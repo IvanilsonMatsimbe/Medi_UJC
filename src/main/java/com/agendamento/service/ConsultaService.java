95bc7a7aa0540f1b953e53544516862204b069c5
Ericazitha <zithaerica3@gmail.com>
2025-06-06 23:23:10 +0200

    adicionar interface EspecialidadeService

---
+++ src/main/java/com/agendamento/service/ConsultaService.java
@@ -0,0 +1,12 @@
+﻿package com.agendamento.service;
+
+import com.agendamento.model.Consulta;
+import java.util.List;
+
+public interface ConsultaService {
+    Consulta agendarConsulta(Consulta consulta);
+    Consulta cancelarConsulta(Integer id, String motivo);
+    List<Consulta> listarTodasConsultas();
+    List<Consulta> listarConsultasPorMedicoId(Integer medicoId);
+    Consulta buscarConsultaPorId(Integer id);
+}
--- src/test/java/com/agendamento/controller/ConsultaService.java
+++
@@ -1,5 +0,0 @@
-package com.agendamento.controller;
-
-public class ConsultaService {
-
-}
