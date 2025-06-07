49fff15746df9ab6bf60e1b79891273e2fb45278
Ivanilson Gomes Ernesto Matsimbe <133059690+IvanilsonMatsimbe@users.noreply.github.com>
2025-06-08 00:31:15 +0200

    Commit final com documentacao em Swagger feita- we did it

--- src/main/java/com/agendamento/security/SecurityConfig.java
+++ src/main/java/com/agendamento/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,18 +16,26 @@ public class SecurityConfig {
     @Bean
 public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .csrf().disable()
+        .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/api/auth/**").permitAll()
-            .requestMatchers("/api/consultas/agendar").hasRole("PACIENTE")
-            .requestMatchers("/api/consultas/cancelar/**").hasAnyRole("PACIENTE", "MEDICO")
-            .requestMatchers("/api/consultas/medico/**").hasRole("MEDICO")
-            .anyRequest().authenticated()
+            .requestMatchers(
+                "/",
+                "/index.html",
+                "/swagger-ui/**",
+                "/v3/api-docs/**",
+                "/swagger-resources/**",
+                "/webjars/**",
+                "/favicon.ico",
+                "/api/auth/**",
+                "/error"
+            ).permitAll()
+            .anyRequest().authenticated() // Ou .permitAll() para testes
         )
-        .httpBasic();
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .formLogin(AbstractHttpConfigurer::disable);
+    
     return http.build();
 }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
